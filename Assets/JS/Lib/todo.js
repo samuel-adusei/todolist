63a16d92361e4d2afe3513b2c76e6d4abd430cdc
samuel-adusei <samuel-adusei@hotmail.com>
2017-04-20 18:34:02 +0100

    added remove and styling

--- Assets/JS/Lib/todo.js
+++ Assets/JS/Lib/todo.js
@@ -1,8 +1,29 @@
 // check off specific todo by clicking 
 
-$("li").click(function() { //effects all "Li"
+$("ul").on("click" ,"li",function() { //effects all "Li"
 	$(this).toggleClass("completed"); //this adds an event and link it to the class
 }); 	// this allows a line when you click on a todo is shows a gray line 
 	
-	
-	
+//click on x to delete
+
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500,function(){
+	//the parent puts brings it to the li
+	$(this).remove();
+	//this allows it to fadeOut 
+	});
+	event.stopPropagation();
+	//this stops the system like a loop
+});
+
+
+$("input[type='text'").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todoText from input
+		var todoText = $(this).val();
+		//create a new li and add to ul
+		$(this).val("");
+		//allows it to change to strring 
+$("ul").append("<li><span>X</span>"	 + todoText + "</li>")
+}
+});
