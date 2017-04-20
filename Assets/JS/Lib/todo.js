fc486e5ee5780e82e790c8ab9049b0c83f1b4189
samuel-adusei <samuel-adusei@hotmail.com>
2017-04-20 15:06:12 +0100

    creating toggle

---
+++ Assets/JS/Lib/todo.js
@@ -0,0 +1,8 @@
+// check off specific todo by clicking 
+
+$("li").click(function() { //effects all "Li"
+	$(this).toggleClass("completed"); //this adds an event and link it to the class
+}); 	// this allows a line when you click on a todo is shows a gray line 
+	
+	
+	
