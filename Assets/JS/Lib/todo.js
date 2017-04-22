f5a0418a4a2e6e7ad768bf6b40b5e9e8d3594598
samuel-adusei <samuel-adusei@hotmail.com>
2017-04-22 15:19:26 +0100

    finished

--- Assets/JS/Lib/todo.js
+++ Assets/JS/Lib/todo.js
@@ -17,13 +17,17 @@ $("ul").on("click", "span", function(event) {
 });
 
 
-$("input[type='text'").keypress(function(event){
+$("input[type='text']").keypress(function(event){
 	if(event.which === 13){
-		//grabbing new todoText from input
+		//grabbing new todo text from input
 		var todoText = $(this).val();
-		//create a new li and add to ul
 		$(this).val("");
-		//allows it to change to strring 
-$("ul").append("<li><span>X</span>"	 + todoText + "</li>")
-}
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
+
+
+$(".fa fa-hourglasss").click(function(){
+	$("input[type='text'").fadeToggle()
 });
